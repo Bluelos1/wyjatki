39da6ace020faf903392a0926b3114bf451620d0
Bluelos1 <s25946@pjwstk.edu.pl>
2022-05-09 20:58:00 +0200

    wyjatki

--- src/pjatk/wyjatki/zadanie4/Main.java
+++ src/pjatk/wyjatki/zadanie4/Main.java
@@ -8,8 +8,11 @@ public class Main {
     }
     public static void osoba(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj imie");
         String imie = scanner.nextLine();
+        System.out.println("podaj nazwisko");
         String nazwisko = scanner.nextLine();
+        System.out.println("podaj rok urodzenia");
         int rokUrodzenia = scanner.nextInt();
 
         try{
